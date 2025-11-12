508648eafd3add17efa56f3ca700468c54b36d3a
Demis Bellot <demis.bellot@gmail.com>
2025-11-12 17:35:11 +0800

    Add API integration

--- MyApp.Client/src/main.tsx
+++ MyApp.Client/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ClientContext, setLinkComponent } from "@servicestack/react"
+import { Link } from 'react-router-dom'
+import { init, client } from './gateway'
+
+// Configure the library to use React Router's Link component
+setLinkComponent(Link)
+init()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <ClientContext.Provider value={client}>
+        <App />
+      </ClientContext.Provider>
   </StrictMode>,
 )
