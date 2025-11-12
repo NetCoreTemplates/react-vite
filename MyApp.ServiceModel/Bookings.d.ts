4cdb3ac89bb2891bab6226d385d219bf1db0547c
Demis Bellot <demis.bellot@gmail.com>
2025-11-12 17:05:41 +0800

    switch to TypeScript generated Bookings

---
+++ MyApp.ServiceModel/Bookings.d.ts
@@ -0,0 +1,67 @@
+/// <reference path="./api.d.ts" />
+export type Config = {
+  prompt:    "New Booking"
+  api:       "~/MyApp.ServiceModel/Bookings.cs"
+  migration: "~/MyApp/Migrations/Migration1000.cs"
+  tip:       "Remove Bookings Feature: npx okai rm Bookings.d.ts",
+}
+
+export enum RoomType {
+  Single,
+  Double,
+  Queen,
+  Twin,
+  Suite,
+}
+
+@Read.route("/bookings","GET")
+@Read.route("/bookings/{Id}","GET")
+@Read.description("Find Bookings")
+@Create.route("/bookings","POST")
+@Create.description("Create a new Booking")
+@Update.notes("Find out how to quickly create a <a class='svg-external' target='_blank' href='https://youtu.be/nhc4MZufkcM'>C# Bookings App from Scratch</a>")
+@Update.route("/booking/{Id}","PATCH")
+@Update.description("Update an existing Booking")
+@Delete.route("/booking/{Id}","DELETE")
+@Delete.description("Delete a Booking")
+@tag("Bookings")
+@icon({svg:"<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path fill='currentColor' d='M16 10H8c-.55 0-1 .45-1 1s.45 1 1 1h8c.55 0 1-.45 1-1s-.45-1-1-1zm3-7h-1V2c0-.55-.45-1-1-1s-1 .45-1 1v1H8V2c0-.55-.45-1-1-1s-1 .45-1 1v1H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-1 16H6c-.55 0-1-.45-1-1V8h14v10c0 .55-.45 1-1 1zm-5-5H8c-.55 0-1 .45-1 1s.45 1 1 1h5c.55 0 1-.45 1-1s-.45-1-1-1z'/></svg>"})
+@notes("Captures a Persons Name & Room Booking information")
+@description("Booking Details")
+@validateHasRole("Employee")
+export class Booking extends AuditBase {
+  @autoIncrement()
+  id: number
+  @Create.description("Name this Booking is for")
+  @validateNotEmpty()
+  name: string
+  roomType: RoomType
+  @validateGreaterThan(0)
+  roomNumber: number
+  @intlDateTime(DateStyle.Long)
+  bookingStartDate: Date
+  @intlRelativeTime()
+  bookingEndDate?: Date
+  @intlNumber({currency:"USD"})
+  @validateGreaterThan(0)
+  cost: decimal
+  @ref({model:"nameof(Coupon)",refId:"nameof(Coupon.Id)",refLabel:"nameof(Coupon.Description)"})
+  @references("typeof(Coupon)")
+  couponId?: string
+  @reference()
+  discount?: Coupon
+  @input({type:"textarea"})
+  notes?: string
+  cancelled?: boolean
+  @reference({selfId:"nameof(CreatedBy)",refId:"nameof(User.UserName)",refLabel:"nameof(User.DisplayName)"})
+  employee: User
+}
+
+@tag("Bookings")
+@icon({svg:"<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path fill='currentColor' d='M2 9.5V4a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v5.5a2.5 2.5 0 1 0 0 5V20a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-5.5a2.5 2.5 0 1 0 0-5zm2-1.532a4.5 4.5 0 0 1 0 8.064V19h16v-2.968a4.5 4.5 0 0 1 0-8.064V5H4v2.968zM9 9h6v2H9V9zm0 4h6v2H9v-2z' /></svg>"})
+export class Coupon extends AuditBase {
+  id: string
+  description: string
+  discount: number
+  expiryDate?: Date
+}
--- MyApp/Migrations/Migration1000.cs
+++ MyApp/Migrations/Migration1000.cs
@@ -1,84 +1,62 @@
 using System.Data;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage;
-using MyApp.Data;
 using ServiceStack;
 using ServiceStack.DataAnnotations;
 using ServiceStack.OrmLite;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using MyApp;
 
 namespace MyApp.Migrations;
 
 public class Migration1000 : MigrationBase
 {
+
+    [Notes("Captures a Persons Name & Room Booking information")]
     public class Booking : AuditBase
     {
         [AutoIncrement]
         public int Id { get; set; }
-        public string Name { get; set; } = default!;
+        public string Name { get; set; }
         public RoomType RoomType { get; set; }
         public int RoomNumber { get; set; }
+        [IntlDateTime(DateStyle.Long)]
         public DateTime BookingStartDate { get; set; }
+        [IntlRelativeTime]
         public DateTime? BookingEndDate { get; set; }
+        [IntlNumber(Currency="USD")]
         public decimal Cost { get; set; }
-        public string? Notes { get; set; }
-        public bool? Cancelled { get; set; }
-
         [References(typeof(Coupon))]
         public string? CouponId { get; set; }
+        public string? Notes { get; set; }
+        public bool? Cancelled { get; set; }
     }
-
-    public class Coupon
+    
+    public class Coupon : AuditBase
     {
-        public string Id { get; set; } = default!;
-        public string Description { get; set; } = default!;
-        public int Discount { get; set; }
-        public DateTime ExpiryDate { get; set; }
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public decimal Discount { get; set; }
+        public DateTime? ExpiryDate { get; set; }
     }
-
+    
     public enum RoomType
     {
-        Queen,
+        Single,
         Double,
+        Queen,
+        Twin,
         Suite,
     }
+    
 
     public override void Up()
     {
         Db.CreateTable<Coupon>();
         Db.CreateTable<Booking>();
-
-        new[] { 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, }.Each(percent => {
-            Db.Insert(new Coupon
-            {
-                Id = $"BOOK{percent}",
-                Description = $"{percent}% off",
-                Discount = percent,
-                ExpiryDate = DateTime.UtcNow.AddDays(30)
-            });
-        });
-
-        CreateBooking(Db, "First Booking!", RoomType.Queen, 10, 100, "BOOK10", "employee@email.com");
-        CreateBooking(Db, "Booking 2", RoomType.Double, 12, 120, "BOOK25", "manager@email.com");
-        CreateBooking(Db, "Booking the 3rd", RoomType.Suite, 13, 130, null, "employee@email.com");
     }
-
-    public void CreateBooking(IDbConnection? db,
-        string name, RoomType type, int roomNo, decimal cost, string? couponId, string by) =>
-        db.Insert(new Booking
-        {
-            Name = name,
-            RoomType = type,
-            RoomNumber = roomNo,
-            Cost = cost,
-            BookingStartDate = DateTime.UtcNow.AddDays(roomNo),
-            BookingEndDate = DateTime.UtcNow.AddDays(roomNo + 7),
-            CouponId = couponId,
-            CreatedBy = by,
-            CreatedDate = DateTime.UtcNow,
-            ModifiedBy = by,
-            ModifiedDate = DateTime.UtcNow,
-        });
 
     public override void Down()
     {
